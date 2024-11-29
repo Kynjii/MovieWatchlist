b08c84cfa2b159d3006932c775acc08bb240bfb8
Dean B <deantburrows88@gmail.com>
2024-11-29 14:51:16 +0100

    added a search based on imdbIDs from the search

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const searchBtn = document.getElementById("search-button")
 const searchEl = document.getElementById("my-search")
 
+let searchResult = []
+
 function handleSearch(e){
     e.preventDefault()
     const baseUrl = "http://www.omdbapi.com/?"
@@ -11,12 +13,69 @@ function handleSearch(e){
         apiKey: key
     })
     const fetchUrl = `${baseUrl}${params.toString()}`
-    console.log(fetchUrl)
+    
 
     fetch(fetchUrl)
         .then(res => res.json())
-        .then(data => console.log(data))        
+        .then(searchData => {
+            const moviesArr = searchData.Search || []
+            searchResult = moviesArr
+            searchTitle()
+            })        
 }
+
+function searchTitle(){
+    let html = ``
+    for (let movie of searchResult){
+        const baseUrl = "http://www.omdbapi.com/?"
+        const key = "f7f81c19"
+        const imdbId = movie.imdbID
+        const params = new URLSearchParams({
+            i: imdbId,
+            apiKey: key
+        })
+        const fetchUrl = `${baseUrl}${params.toString()}`
+
+        fetch(fetchUrl)
+            .then(res => res.json())
+            .then(data => {
+                    html += `
+                        <div class="result-card-container">
+                            <img src="${data.Poster}" class="poster" alt="A poster for the movie ${data.Title}">
+                            <h2>${data.Title}</h2>
+                            <i class="fa-solid fa-star"></i>
+                            <p class="imdb-rating" aria-label="imdb rating">${data.imdbRating}</p>
+                            <p class="runtime" aria-label="runtime">${data.Runtime}</p>
+                            <p class="genre" aria-label="genre">${data.Genre}</p>
+                                <div class="add-button">
+                                    <i class="fa-solid fa-circle-plus"></i>   
+                                    <p>Watchlist</p>               
+                                </div>
+                            <p class="plot-container" id="plot-container">
+                                ${data.Plot}
+                            </p>
+                        </div>
+                    `
+                
+                document.getElementById("search-results-container").innerHTML = html
+                     })
+                
+            }
+    }
+
+
+
 
 searchBtn.addEventListener('click', handleSearch)
 
+
+
+
+
+
+
+
+
+
+
+ 
